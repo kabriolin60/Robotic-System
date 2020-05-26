0acd315c442324fdfdb6b709e652e0d11d1eb876
kabriolin60 <57366657+kabriolin60@users.noreply.github.com>
2020-05-26 10:12:29 +0200

    Update 1_Servos.h

--- Workspaces/MultiWorkSpaces_2021/MultiFonction_2021_V0/Actionneurs_Capteurs/1_Servos.h
+++ Workspaces/MultiWorkSpaces_2021/MultiFonction_2021_V0/Actionneurs_Capteurs/1_Servos.h
@@ -9,6 +9,9 @@
 #define _1_SERVOS_H_
 
 void _1_Servos_Init(void);
+
+void _1_Servos_Set_Destination(byte id, unsigned short desti, unsigned short time);
+
 void _1_Servos_Calcul_Ramp(struct st_SERVO *Servo, unsigned short Temps);
 void _1_Servos_MAJ_Consigne_Servo(struct st_SERVO *s);
 void _1_Servos_Update_Servo(void *pvParameters);
