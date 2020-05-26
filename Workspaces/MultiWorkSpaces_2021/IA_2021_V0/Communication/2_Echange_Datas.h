8bdf5578039903ffcec749c328845042dea69560
kabriolin60 <57366657+kabriolin60@users.noreply.github.com>
2020-05-26 11:30:42 +0200

    Update 2_Echange_Datas.h

--- Workspaces/MultiWorkSpaces_2021/IA_2021_V0/Communication/2_Echange_Datas.h
+++ Workspaces/MultiWorkSpaces_2021/IA_2021_V0/Communication/2_Echange_Datas.h
@@ -36,4 +36,6 @@ void _2_Comm_Send_Log_Message(char* str, enum Logger_Debug_Color color, enum enu
 
 void _2_Communication_Boards_Status(void* pvParameters);
 
+void _2_Comm_Send_Servos_Destinations(struct st_Destination_Servos* destination, enum enum_canal_communication canal);
+
 #endif /* 2_COMMUNICATION_2_ECHANGE_DATAS_H_ */
