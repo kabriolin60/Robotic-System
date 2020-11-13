621daa292013c5d14f2d56493a5ba42b5874b8e0
kabriolin60 <57366657+kabriolin60@users.noreply.github.com>
2020-11-13 19:23:41 +0100

    Logger fonctionnel Debug Channels fonctionnel

---
+++ TEST/TEST_IHM/test_IHM/Assets/Scripts/Logger/Add_New_Line.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Add_New_Line : MonoBehaviour
+{
+    public GameObject Line_Contener;
+    public GameObject Line_Prefab;
+
+    public GameObject Channel_Toggle;
+
+    public void Start()
+    {
+        //string time = $"{System.DateTime.Now.Hour}:{System.DateTime.Now.Minute}:{System.DateTime.Now.Second}:{System.DateTime.Now.Millisecond}";
+        //Add_New_Logger_Line(time, 3, Color.green, "Toto est passé par ici");
+    }
+
+    public void Add_New_Logger_Line(string time, int Channel, Color color, string text)
+    {
+        Logger_Channel[] channels = Channel_Toggle.GetComponentsInChildren<Logger_Channel>();        
+
+        GameObject go = Instantiate(Line_Prefab, Line_Contener.transform) as GameObject;
+        go.GetComponent<Logger_Line>().SetNewLine(time, Channel, color, text);
+
+        foreach (Logger_Channel _channel in channels)
+        {
+            if (_channel.Channel_id == Channel)
+            {
+                if (_channel.GetComponentInParent<Toggle>().isOn == false)
+                {
+                    //Debug.Log("Channel desabled");
+                    go.SetActive(false);
+                }
+            }
+        }
+    }
+}
---
+++ TEST/TEST_IHM/test_IHM/Assets/Scripts/Logger/Logger_Line.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Logger_Line : MonoBehaviour
+{
+    public int Channel_id;  //Id du channel pour savoir s'il doit être affiché ou non   
+
+    public GameObject Time_Text;
+    public GameObject Channel_Text;
+    public GameObject Text;    
+
+    public void SetNewLine(string time, int channel, Color color, string text)
+    {
+        this.Channel_id = channel;
+
+        Time_Text.GetComponent<TextMeshProUGUI>().text = time;
+        Channel_Text.GetComponent<TextMeshProUGUI>().text = $"Ch {channel}";
+        Text.GetComponent<TextMeshProUGUI>().text = text;
+
+        Text.GetComponent<TextMeshProUGUI>().color = color;
+    }
+}
