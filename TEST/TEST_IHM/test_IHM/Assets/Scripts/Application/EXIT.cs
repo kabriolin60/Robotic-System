09eb2542044c9aa5509025815bf85ff7e9c6fb82
kabriolin60 <57366657+kabriolin60@users.noreply.github.com>
2020-11-14 10:11:43 +0100

    Amélioration affichage des onglets Fonctionnalité du bouton Exit

---
+++ TEST/TEST_IHM/test_IHM/Assets/Scripts/Application/EXIT.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EXIT : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Exit_Application()
+    {
+        // save any game data here
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+}
