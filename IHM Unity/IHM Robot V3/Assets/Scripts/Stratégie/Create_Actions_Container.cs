5e7255350ccef0486b7ab49628bed7d2f9e7de48
kabriolin60 <57366657+kabriolin60@users.noreply.github.com>
2020-12-05 16:00:15 +0100

    ajout onglet déplacements

--- IHM Unity/IHM Robot V3/Assets/Scripts/Stratégie/Create_Actions_Container.cs
+++ IHM Unity/IHM Robot V3/Assets/Scripts/Stratégie/Create_Actions_Container.cs
@@ -12,6 +12,7 @@ public class Create_Actions_Container : MonoBehaviour
     public GameObject Onglet_Prefab;
     public GameObject Strategie_Panel_Prefab;
     public GameObject PID_Panel_Prefab;
+    public GameObject Deplacement_Panel_Prefab;
 
     private List<GameObject> list_Panels = new List<GameObject>(); 
 
@@ -23,6 +24,9 @@ public class Create_Actions_Container : MonoBehaviour
 
         //Panel de PID
         Create_PID_Panel(false).SetActive(false);
+
+        //Panel de Déplacements
+        Create_Deplacement_Panel(false).SetActive(false);
     }
 
     private GameObject Create_Strategie_Panel(bool is_tabOn)
@@ -66,6 +70,28 @@ public class Create_Actions_Container : MonoBehaviour
         go_Panel.GetComponentInChildren<TextMeshProUGUI>().text = "PIDs";
         //Set le nom du panel
         go_contenu.name = $"Tab PID";
+
+        return go_contenu;
+    }
+
+    private GameObject Create_Deplacement_Panel(bool is_tabOn)
+    {
+        GameObject go_Panel = Instantiate(Onglet_Prefab, Switch_Holder.transform) as GameObject;
+        go_Panel.GetComponent<Toggle>().isOn = is_tabOn;
+        go_Panel.GetComponent<Toggle>().group = Switch_Holder.GetComponent<ToggleGroup>();
+
+        GameObject go_contenu = Instantiate(Deplacement_Panel_Prefab, Contenu_Panel.transform) as GameObject;
+        list_Panels.Add(go_contenu);
+
+        //Associe le panel à cet onglet
+        go_Panel.GetComponent<Toggle>().onValueChanged.AddListener(go_contenu.SetActive);
+
+        //Set le nom de l'onglet
+        go_Panel.name = $"Onglet Deplacements";
+        //Set onglet button name
+        go_Panel.GetComponentInChildren<TextMeshProUGUI>().text = "Deplacements";
+        //Set le nom du panel
+        go_contenu.name = $"Tab Deplacements";
 
         return go_contenu;
     }
