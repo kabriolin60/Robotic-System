7a79664fd8e62e9a3f7d71b249f52e8df25a0a9e
kabriolin60 <57366657+kabriolin60@users.noreply.github.com>
2020-12-21 16:31:28 +0100

    ajout scripts de manipulation des articulations et des objets

---
+++ IHM Unity/IHM Robot V3/Assets/Scripts/Actionneurs/Articulation.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Articulation : MonoBehaviour
+{
+    public enum Type_Joint
+    {
+        Free,
+        Lineaire,
+        Rotation
+    }
+
+    public enum Repere_Joint
+    {
+        Local,
+        Parent,
+        World
+    }
+
+    public Type_Joint type_Joint;
+    public Repere_Joint repere_Joint;
+    public Transform Connected_Body;
+
+    public Vector3 Anchor;
+    public Vector3 Axis;
+
+    public bool use_automatic_parent_anchor = true;
+    public Vector3 Connected_Anchor;
+
+
+    private HingeJoint hinge_joint;
+
+    void OnDrawGizmosSelected()
+    {
+        Vector3 direction = new Vector3();
+        Ray joint_ray;
+        switch (type_Joint)
+        {
+            case Type_Joint.Free:
+                break;
+
+            case Type_Joint.Lineaire:            
+                Gizmos.color = Color.green;               
+                direction = Connected_Body.TransformVector(Axis);
+
+                joint_ray = new Ray(this.transform.TransformPoint(Anchor), direction*3.0f);
+                Gizmos.DrawRay(joint_ray);
+                break;
+
+            case Type_Joint.Rotation:
+                Gizmos.color = Color.green;
+                direction = this.transform.TransformVector(Axis);
+
+                joint_ray = new Ray(this.transform.TransformPoint(Anchor), direction * 3.0f);
+                Gizmos.DrawRay(joint_ray);
+                break;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        switch (type_Joint)
+        {
+            default:
+                break;
+
+            case Type_Joint.Rotation:
+                this.hinge_joint = this.gameObject.AddComponent<HingeJoint>();
+                hinge_joint.connectedBody = Connected_Body.GetComponent<Rigidbody>();
+                hinge_joint.anchor = this.Anchor;
+                hinge_joint.axis = this.Axis;
+
+                if(!use_automatic_parent_anchor)
+                {
+                    hinge_joint.autoConfigureConnectedAnchor = false;
+                    hinge_joint.connectedAnchor = this.Connected_Anchor;
+                }
+
+                this.GetComponent<Rigidbody>().useGravity = false;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ IHM Unity/IHM Robot V3/Assets/Scripts/Actionneurs/Pince.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pince : MonoBehaviour
+{
+    public LayerMask target_layer;
+    public Vector3 Anchor;
+    public Vector3 Axis;
+    public float Raycast_Length;
+
+    public bool Activate = false;
+
+    private FixedJoint fixed_Joint;
+    private Transform grabed_Object;
+
+    private void Awake()
+    {
+        if (this.GetComponent<Rigidbody>() == null)
+        {
+            this.gameObject.AddComponent<Rigidbody>();
+            this.GetComponent<Rigidbody>().isKinematic = false;
+            this.GetComponent<Rigidbody>().useGravity = false;
+            this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;            
+        }
+
+        if(this.transform.parent.GetComponent<Rigidbody>() == null)
+        {
+            this.transform.parent.gameObject.AddComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Vector3 direction = new Vector3();
+
+        Gizmos.color = Color.yellow;
+        direction = this.transform.TransformVector(Axis);
+
+        Gizmos.DrawLine(this.transform.TransformPoint(Anchor), this.transform.TransformPoint(Anchor) + direction.normalized * Raycast_Length);
+    }
+
+    void OnDrawGizmosSelected2()
+    {
+        Vector3 direction = new Vector3();
+
+        Gizmos.color = Color.yellow;
+        direction = this.transform.TransformVector(Axis);
+
+        Gizmos.DrawRay(this.transform.TransformPoint(Anchor), direction * Raycast_Length);
+    }
+
+    public void Activate_Actionneur(bool state)
+    {
+        Activate = state;
+
+        if(!state)
+        {
+            grabed_Object = null;
+            this.gameObject.GetComponent<FixedJoint>().connectedBody = null;
+        }
+    }
+    
+
+    RaycastHit hit;
+    void FixedUpdate()
+    {
+        if (this.Activate)
+        {
+            //Does the ray intersect any object in the range, according to layer mask
+            if (Physics.Raycast(new Ray(this.transform.TransformPoint(Anchor), this.transform.TransformVector(this.Axis)), out hit, this.Raycast_Length, this.target_layer))
+            {
+                Vector3 hit_length = new Vector3();
+                hit_length = TransformExtensions.TransformPointUnscaled(this.transform, hit.point) - this.transform.TransformPoint(Anchor);
+                Debug.DrawLine(this.transform.TransformPoint(Anchor), hit.point, Color.red);
+
+                if (grabed_Object == null)
+                {
+                    grabed_Object = hit.transform;
+                    this.fixed_Joint = this.gameObject.AddComponent<FixedJoint>();
+                    this.fixed_Joint.connectedBody = grabed_Object.GetComponent<Rigidbody>();
+                }
+            }
+        }
+        else
+        {
+            grabed_Object = null;
+        }
+    }
+}
+
+
+public static class TransformExtensions
+{
+    public static Vector3 TransformPointUnscaled(this Transform transform, Vector3 position)
+    {
+        var localToWorldMatrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+        return localToWorldMatrix.MultiplyPoint3x4(position);
+    }
+
+    public static Vector3 InverseTransformPointUnscaled(this Transform transform, Vector3 position)
+    {
+        var WorldToLocalMatrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one).inverse;
+        return WorldToLocalMatrix.MultiplyPoint3x4(position);
+    }
+}
